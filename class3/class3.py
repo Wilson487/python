02b7a77ad27637c9e8ffe8c51473ff788e580a66
Wilson487 <wilsonchu2012@gmail.com>
2023-06-30 21:26:15 +0800

    class3

---
+++ class3/class3.py
@@ -0,0 +1,23 @@
+# '''
+# len算字串裡的字有多少
+# type告訴我資料物件的種類
+# int()裡面的東東變整數
+# float把()裡面的東東變小數
+# bool()裡面的東東變布林
+# str()裡面的東東變字串
+# '''
+#ctrl+?=快速註解的方式
+print(False)
+print(-1.000)
+print(123)
+print('hello world')
+print('黃金體驗,石之海,性感手槍,紫煙,世界,白金之星')
+#JOJO
+#提示使用者輸入資料,輸出返回:使用者輸入的資料,字串型態
+print('開始輸入')
+input()
+print('輸入結束')
+input('輸入資料')
+a = input
+print(a)
+print(input)
