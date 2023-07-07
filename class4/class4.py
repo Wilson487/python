f667a20305f358e1b342f83ddfc4df464bd1760f
Wilson487 <wilsonchu2012@gmail.com>
2023-07-07 21:10:11 +0800

    hw4.py

---
+++ class4/class4.py
@@ -0,0 +1,12 @@
+'''
+#print(input("輸入數字"))  是OK的指令
+# a=input("輸入自己的年齡")
+# print("你的年齡是")print(a)  <---這是錯的哈哈
+a = input("輸入自己的年齡")
+print("你的年齡是" + a + "歲")
+'''
+r = input("半徑")
+r = float(r)
+pi = 3.14
+A = pi * r**2
+print(A)
