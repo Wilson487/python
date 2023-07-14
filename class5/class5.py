cdfa66ddfd28ec2e6db22787fab66d7ff3fa1ffd
Wilson487 <wilsonchu2012@gmail.com>
2023-07-14 21:22:35 +0800

    hw5.py

---
+++ class5/class5.py
@@ -0,0 +1,29 @@
+'''
+name = 'python'
+age = '5487'
+print('我叫' + name + '我今年' + age + '歲')
+'''
+'''
+name = '野原新之助'
+age = '5'
+print('我叫' + name + '我今年' + age + '歲')
+'''
+'''
+name = '空調承太郎'
+age = '5'
+print('我叫' + name)
+'''
+'''
+%s=字串的變數
+%d=整數的變數
+%f=小數的變數
+'''
+# name = 'python'
+# age = 5487
+# print('我叫%s我今年%d歲'%(name,age))
+
+name = 'python'
+age = input("請輸入年齡")
+age = int(age)
+age = age + 10
+print('我十年後%d歲' % (age))
