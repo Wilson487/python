15a85a041a4bc8e5840b04bea2d13dfd7d20ef71
Wilson487 <wilsonchu2012@gmail.com>
2023-07-14 19:57:29 +0800

    HW4.PY

--- class4/hw4.py
+++ class4/hw4.py
@@ -7,6 +7,8 @@ print("I am " + old)
 2.input:12
 3.Output:I am 12
 """
+WW = input('請輸入你的智商')
+print('你這個大傻逼，居然只有' + WW + '點點智商')
 """
 請使用者輸入身高(公尺)h以及體重(公斤)w
 透過下面公式計算出BMI數值並顯示計算結果
@@ -16,7 +18,10 @@ EX:
 請輸入體重:50
 你的BMI為17.301038062283737
 """
+
 w = input("請輸入體重:")
 h = input("請輸入身高(公尺):")
+h = float(h)
+W = float(W)
 bmi = w / h**2
 print("你的BMI為" + bmi)
