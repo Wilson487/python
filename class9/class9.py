0bff7e1a77f3d316a65e4c7cd26147fa11a766ae
Wilson487 <wilsonchu2012@gmail.com>
2023-08-25 19:34:55 +0800

    hw9

---
+++ class9/class9.py
@@ -0,0 +1,41 @@
+import turtle
+'''
+turtle.left(9000)  #int or float
+turtle.right(9000)  #int or float
+turtle.forward(10)  #int or float
+turtle.backward(9000)  #int or float
+turtle.done()
+'''
+'''
+turtle.forward(150)  #int or float
+turtle.left(90)  #int or floa
+turtle.forward(150)  #int or float
+turtle.left(90)  #int or floaturtle.forward(150)  #int or float
+turtle.forward(150)  #int or float
+turtle.left(90)  #int or floa
+turtle.forward(150)  #int or float
+turtle.left(90)  #int or floaturtle.forward(150)  #int or float
+turtle.done()
+'''
+# turtle.forward(150)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(150)  #int or float
+# turtle.left(60)  #int or floaturtle.forward(150)  #int or float
+# turtle.forward(150)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(150)  #int or float
+# turtle.left(60)  #int or float
+# turtle.forward(150)  #int or float
+# turtle.left(60)  #int or floaturtle.forward(150)  #int or float
+# turtle.forward(150)  #int or float
+# turtle.done()
+# for i in range(10):
+#     print(i)
+#for迴圈變數 in range (執行次數):
+#要重複的程式
+#hello hello hello hello hello
+for i in range(2, 10, 2):
+    print(i)
+#for迴圈變數 in range (起始值，終止值，變化值):
+#要重複的程式
+#hello hello hello hello hello
---
+++ class9/hw9.py
@@ -0,0 +1,21 @@
+'''
+作業1.
+使用turtle模組與for 劃出一個正六邊形
+作業2.
+做一個可以列印出0-100之間3的倍數的機器。
+ex:
+3
+6
+9
+12
+....
+'''
+# turtle.forward(150)  
+#  turtle.left(60) 
+# for i in range(3, 100, 3):
+#     print(i)
+import turtle
+for i in range(0,6):
+turtle.forward(150)
+turtle.left(60)
+turtle.done()
